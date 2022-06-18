8ad4882c22fb491497d248e16759ddabb88a057c
Muhammad Haseeb <88443778+mhaseebmohal@users.noreply.github.com>
2022-06-18 16:42:30 +0500

    Update and rename setting1.8.js to setting1.9.js

--- setting1.9.js
+++ setting1.9.js
@@ -1,4 +1,31 @@
-function labnolIframe(div) {
+/* rating widget */
+//<![CDATA[
+(function(d, t, e, m){ 
+    // Async Rating-Widget initialization.
+    window.RW_Async_Init = function(){          
+        RW.init({
+            huid: "484262",
+            uid: "7e794e29e2f7d8a0e6db4315d73a9750",
+            options: { "style": "oxygen" } 
+        });
+        RW.render();
+    };
+        // Append Rating-Widget JavaScript library.
+    var rw, s = d.getElementsByTagName(e)[0], id = "rw-js",
+        l = d.location, ck = "Y" + t.getFullYear() + 
+        "M" + t.getMonth() + "D" + t.getDate(), p = l.protocol,
+        f = ((l.search.indexOf("DBG=") > -1) ? "" : ".min"),
+        a = ("https:" == p ? "secure." + m + "js/" : "js." + m);
+    if (d.getElementById(id)) return;              
+    rw = d.createElement(e);
+    rw.id = id; rw.async = true; rw.type = "text/javascript";
+    rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
+    s.parentNode.insertBefore(rw, s);
+    }(document, new Date(), "script", "rating-widget.com/"));
+
+
+/* youtube lazyload */
+  function labnolIframe(div) {
     var iframe = document.createElement('iframe');
     iframe.setAttribute('src', 'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0');
     iframe.setAttribute('frameborder', '0');
@@ -6,7 +33,6 @@ function labnolIframe(div) {
     iframe.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
     div.parentNode.replaceChild(iframe, div);
   }
-
   function initYouTubeVideos() {
     var playerElements = document.getElementsByClassName('youtube-player');
     for (var n = 0; n < playerElements.length; n++) {
@@ -25,5 +51,6 @@ function labnolIframe(div) {
       playerElements[n].appendChild(div);
     }
   }
+document.addEventListener('DOMContentLoaded', initYouTubeVideos);
 
-  document.addEventListener('DOMContentLoaded', initYouTubeVideos);
+//]]>

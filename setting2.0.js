3275ec04d5d4827717ccc94253ac48f969f5b77f
Muhammad Haseeb <88443778+mhaseebmohal@users.noreply.github.com>
2022-06-18 16:44:22 +0500

    Update and rename setting1.9.js to setting2.0.js

--- setting2.0.js
+++ setting2.0.js
@@ -22,9 +22,10 @@
     rw.src = p + "//" + a + "external" + f + ".js?ck=" + ck;
     s.parentNode.insertBefore(rw, s);
     }(document, new Date(), "script", "rating-widget.com/"));
-
+//]]>
 
 /* youtube lazyload */
+//<![CDATA[
   function labnolIframe(div) {
     var iframe = document.createElement('iframe');
     iframe.setAttribute('src', 'https://www.youtube.com/embed/' + div.dataset.id + '?autoplay=1&rel=0');
@@ -52,5 +53,4 @@
     }
   }
 document.addEventListener('DOMContentLoaded', initYouTubeVideos);
-
 //]]>

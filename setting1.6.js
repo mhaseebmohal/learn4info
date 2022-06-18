481e2160fb755bf164773ad7dca71303880a8fde
Muhammad Haseeb <88443778+mhaseebmohal@users.noreply.github.com>
2022-06-18 13:11:30 +0500

    Update setting1.6.js

--- setting1.6.js
+++ setting1.6.js
@@ -27,3 +27,39 @@ function labnolIframe(div) {
   }
 
   document.addEventListener('DOMContentLoaded', initYouTubeVideos);
+
+
+
+const element = document.getElementById(&quot;topad12&quot;);
+document.getElementById(&quot;topad1&quot;).innerHTML = element.innerHTML;     
+  
+const elementb = document.getElementById(&quot;bottomad12&quot;);
+document.getElementById(&quot;bottomad1&quot;).innerHTML = elementb.innerHTML;
+
+  
+  
+function insertAfter(Ads, targetAds) {
+    var inarticlead = targetAds.parentNode;
+    if (inarticlead.lastChild == targetAds) {
+        inarticlead.appendChild(Ads);
+    } else {
+        inarticlead.insertBefore(Ads, targetAds.nextSibling);
+    } 
+}
+var targetAds = document.getElementById(&quot;post-body&quot;);
+var inarticleAd1 = document.getElementById(&quot;after-paragraph33&quot;);
+var showinarticleAd1 = targetAds.getElementsByTagName(&quot;p&quot;);
+if (showinarticleAd1.length &gt; 0) {
+    insertAfter(inarticleAd1, showinarticleAd1[3]);
+}
+
+var inarticleAd2 = document.getElementById(&quot;after-paragraph77&quot;);
+var showinarticleAd2 = targetAds.getElementsByTagName(&quot;h2&quot;);
+if (showinarticleAd2.length &gt; 0) {
+    insertAfter(inarticleAd2, showinarticleAd2[2]);
+}
+    var inarticleAd3 = document.getElementById(&quot;after-paragraph66&quot;);
+var showinarticleAd3 = targetAds.getElementsByTagName(&quot;li&quot;);
+if (showinarticleAd3.length &gt; 0) {
+    insertAfter(inarticleAd3, showinarticleAd3[0]);
+}

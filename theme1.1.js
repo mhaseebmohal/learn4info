94bd91802a4bc9a05990a192d5e8b85d5a30ebe7
Muhammad Haseeb <88443778+mhaseebmohal@users.noreply.github.com>
2022-07-17 15:21:53 +0500

    Update and rename theme1.1.js to theme1.2.js

--- theme1.1.js
+++
@@ -1,4 +0,0 @@
-/* cookie consent */
-/*<![CDATA[*/ var lazyload = !1;window.addEventListener("scroll",function () {((0 != document.documentElement.scrollTop && !1 === lazyload) || (0 != document.body.scrollTop && !1 === lazyload)) &&(!(function () {
-var K2C_box=document.querySelector("#K2C_box"),Cookie_btn=document.querySelector("#Cookie_btn"),ckErrMes="Cookie can't be set! Please unblock this site from the cookie setting of your browser.";if(null!=K2C_box){Cookie_btn.onclick=()=>{document.cookie="CookieConsentByFineshop=Accepted; max-age=2592000; path=/",document.cookie?K2C_box.classList.add("acptd"):alert(ckErrMes)};let e=document.cookie.indexOf("CookieConsentByFineshop=Accepted");-1!=e?K2C_box.classList.add("k2hide"):K2C_box.classList.remove("k2hide")}
-})(),(lazyload = !0));},!0); /*]]>*/
